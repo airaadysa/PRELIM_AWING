40dce69f97fb28ad34e2aa511687740220e23e00
airaadysa <ajbawing@pcu.edu>
2024-02-22 21:44:32 +0800

    exam part 2

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Landing Page</title>
+    <meta name="description" content="The small framework with powerful features">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/png" href="/favicon.ico">
+
+    <!-- STYLES -->
+
+    <style >
+       body{
+        font-family: helvetica;
+        background-color: #2f3242;
+       }
+       .maim-menu{
+        float: right;
+        padding: 20px 0 0 0;
+        margin-bottom: 100px;
+
+       }
+       li{
+        float: left;
+        list-style-type: none;
+        margin: 0 0 0 30px;
+       }
+       a{
+        padding: 10px 15px;
+        color: #333;
+        color: rgba(255, 255, 255, 0.8);
+        text-decoration: none;
+        font-weight: bold;
+
+       }
+       a:hover, a:focus{
+        color: #fff;
+        background: #fff;
+        background: rgba(255, 255, 255, 0.2);
+       }
+
+       .container{
+        
+        background-color: #2f3242;
+     
+       }
+    </style>
+</header>
+
+    <nav>
+        <ul class="main-menu">
+            <li><a href="about"> About</a> <li>
+            <li><a href="service"> Service</a> <li>
+            <li><a href="work"> Work</a> <li>
+            <li><a href="contact"> About</a> <li>
+            
+    </nav>
+
+</header>
+
+<body>
+   <div class="container">
+       <h1> WELCOME </h1>
+       <p> welcome to the website </p>
+
+       <div class="button">
+            <a class="link"> Sign up </a>
+            <a class="link"> Log in </a>
+       </div>
+   </div>
+
+</body>
+</html>
